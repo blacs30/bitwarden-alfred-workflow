b84a4f4c3e1a7420497c264405adfc9489a11474
blacs30 <github@lisowski-development.com>
2020-10-30 00:59:27 +0100

    use int for consts, linter complains otherwise with iota and unused consts

--- crypt.go
+++ crypt.go
@@ -98,13 +98,13 @@ func Decrypt() ([]byte, error) {
 // as well as this repo https://github.com/mvdan/bitw
 // and https://github.com/philhug/bitwarden-client-go
 const (
-	AesCbc256_B64                     = iota
-	AesCbc128_HmacSha256_B64          = iota
-	AesCbc256_HmacSha256_B64          = iota
-	Rsa2048_OaepSha256_B64            = iota
-	Rsa2048_OaepSha1_B64              = iota
-	Rsa2048_OaepSha256_HmacSha256_B64 = iota
-	Rsa2048_OaepSha1_HmacSha256_B64   = iota
+	AesCbc256_B64 = 0
+	//AesCbc128_HmacSha256_B64          = 1
+	AesCbc256_HmacSha256_B64 = 2
+	//Rsa2048_OaepSha256_B64            = 3
+	//Rsa2048_OaepSha1_B64              = 4
+	//Rsa2048_OaepSha256_HmacSha256_B64 = 5
+	//Rsa2048_OaepSha1_HmacSha256_B64   = 6
 )
 
 type CipherString struct {
